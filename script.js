6971a14346800f705fc212450348a1178e64a9c3
Watotacho <watotacho@gmail.com>
2021-03-22 08:23:49 +0300

    second commit

---
+++ script.js
@@ -0,0 +1,21 @@
+function fruit(fruitName,quantity){
+    var total=quantity*30.00;
+
+    return quantity+" "+fruitName+" for KES "+total;
+}
+console.log (fruit("oranges",2));
+fruit();
+
+
+// var person={
+// name:"Daniel",
+// colour:"black",
+// age:27,
+// }
+// console.log(person.name)
+// function car(){
+//     var brand="prado",
+//     var color="blue", }
+
+// }
+// car()
